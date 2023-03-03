b73e92a21cf44ede56c917156ee73a69c2a2b35d
rkrasnozhon <r.s.roman@hotmail.com>
2023-03-03 17:22:43 +0100

    add chart.js for graphic line/winston for log file/ function data real time

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const spi = require('spi-device');
 const express = require('express');
-
+const fs = require("fs");
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const winston = require('winston');
+
+
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "test-log.log"})
+  ]
+  });
 
 const config = {
   mode: 1,
   chipSelect: 0,
   maxSpeedHz: 1000000,
   bitOrder: spi.ORDER_MSB_FIRST,
-  bitsPerWord: 8
+  bitsPerWord: 8,
 };
 
+let data;
+
 const device = spi.open(0, 0, config, err => {
-  
+  setInterval(() => {
   const message = [{
     sendBuffer: Buffer.from([0xff, 0xff]),
     receiveBuffer: Buffer.alloc(2),
@@ -24,32 +42,83 @@ const device = spi.open(0, 0, config, err => {
 
   device.transfer(message, (err, message) => {
   if (err) throw err;
-  console.log(((message[0].receiveBuffer[0] << 8 )+ message[0].receiveBuffer[1]).toString(2));
-  const error = ((0b01000000)&(message[0].receiveBuffer[0])) >> 6;
-  const parity = ((0b10000000)&(message[0].receiveBuffer[0])) >> 7;
   
   message[0].receiveBuffer[0] = (0b00111111)&message[0].receiveBuffer[0];
   
-    const data = (message[0].receiveBuffer[0] << 8 )+ message[0].receiveBuffer[1];
+  let data = (message[0].receiveBuffer[0] << 8 )+ message[0].receiveBuffer[1];
+  let error = ((0b01000000)&(message[0].receiveBuffer[0])) >> 6;
+  let parity = ((0b10000000)&(message[0].receiveBuffer[0])) >> 7;
+  //~ let warning = ;
+  //~ let driverState = ;
+  //~ let workingMode = ;
+  //~ let motionOnGoing = ;
+  //~ let onTargetPosition = ;
+  let actualPosition = data;
+  //~ let actualTorque = ;
+  let actualFrequency = 3*data;
+    
   
+    console.log(((message[0].receiveBuffer[0] << 8 )+ message[0].receiveBuffer[1]).toString(2));
     console.log(`Data: ${data}`);
     console.log(`Error: ${error}`);
     console.log(`Parity: ${parity}`);
+    console.log(`Actual position: ${actualPosition}`);
+    console.log(`Actual frequency: ${actualFrequency}`);
 
-    app.get('/spi-data', (req, res) => {
-      res.json({
-        data,
-        error,
-        parity
-      });
+   setInterval(() => {
+  logger.log('info', `Data: ${data}`);
+}, 10);
+    
+    io.emit('data', {    
+      data: data,
+      parity: parity,
+      error: error,
+      //~ warning: warning,
+      //~ driverState: driverState,
+      //~ workingMode: workingMode,
+      //~ motionOnGoing: motionOnGoing,
+      //~ onTargetPosition: onTargetPosition,
+      actualPosition: actualPosition,
+      //~ actualTorque: actualTorque,     
+      actualFrequency: actualFrequency,
     });
+  });
+},100);
+});  
+   
+
+   
+   
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('Client is on');
+  socket.on('', () => {
+    console.log('Data received');
+    const message = {
+      data: data,
+      parity: parity,
+      error: error,
+      //~ warning: warning,
+      //~ driverState: driverState,
+      //~ workingMode: workingMode,
+      //~ motionOnGoing: motionOnGoing,
+      //~ onTargetPosition: onTargetPosition,
+      actualPosition: actualPosition,
+      //~ actualTorque: actualTorque,     
+      actualFrequency: actualFrequency,
+    };
+    io.emit('data', message);
+  });
+  
+socket.on('disconnect', () => {
+  console.log('Client disconnected');
   });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000')
-})
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
